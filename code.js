edb06ce0e91625f22a953072edc2a72e9fc052df
Vinícius Santos <3izuna@gmail.com>
2026-03-03 13:21:04 -0300

    Adding images and the functions to change the image

--- code.js
+++ code.js
@@ -1,6 +1,11 @@
 let imgChange = document.getElementById('slider-area')
+imgChange.style.backgroundImage = 'url(./assets/img/cartoonOne.jpg)'
 
-const prevImg = () => imgChange.style.backgroundImage = `url()`
+const prevImg = () => {
+    imgChange.style.backgroundImage = 'url(./assets/img/cartoonOne.jpg)'
+}
 
+const nextImg = () => {
+    imgChange.style.backgroundImage = 'url(./assets/img/cartoonTwo.jpg)'
+}
 
-const nextImg = () => imgChange.style.backgroundImage
